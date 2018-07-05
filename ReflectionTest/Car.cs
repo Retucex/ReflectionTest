4e6efaeb40f23918f7cfed2b368ef4d08a37df84
Samuel Leclerc <sam.h.leclerc@gmail.com>
2018-07-04 22:44:43 -0400

    beginning of something

---
+++ ReflectionTest/Car.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionTest
+{
+	public class Car
+	{
+		public int Year { get; set; }
+		public bool Running { get; private set; }
+
+		public Car(int year, bool start)
+		{
+			Year = year;
+			Running = start;
+		}
+
+		public Car(int year) : this(year, false) { }
+
+		public Car(bool start) : this(0, start) { }
+
+		public Car() : this(0, false) { }
+		
+		[Findable]
+		public void Start()
+		{
+			if(Running)
+			{
+				Console.WriteLine("Car is already running!");
+			}
+			else
+			{
+				Running = true;
+				Console.WriteLine("Car has started.");
+			}
+		}
+
+		[Findable]
+		public void Stop()
+		{
+			if (Running)
+			{
+				Running = false;
+				Console.WriteLine("Car has stopped");
+			}
+			else
+			{
+				Console.WriteLine("Car is not running!");
+			}
+		}
+	}
+}
--- ReflectionTest/Program.cs
+++ ReflectionTest/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,20 @@ namespace ReflectionTest
 	{
 		static void Main(string[] args)
 		{
+			byte[] assemblyBytes = File.ReadAllBytes("ReflectionTest.exe");
+			Assembly assembly = Assembly.Load(assemblyBytes);
+
+			var methods = assembly.GetTypes()
+								.SelectMany(t => t.GetMethods())
+								.Where(m => m.GetCustomAttributes<FindableAttribute>().Any())
+								.ToArray();
+
+			foreach(var m in methods)
+			{
+				m.Invoke(Activator.CreateInstance(m.DeclaringType), new Object[] { });
+			}
+
+			Console.ReadKey();
 		}
 	}
 }
